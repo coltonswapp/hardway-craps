acac81628174387174f0271119f314996f8b4181
Colton Swapp <coltonbswapp@gmail.com>
2025-12-22 17:48:56 -0700

    Initial Commit

---
+++ hardway-craps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hardway-craps
+//
+//  Created by Colton Swapp on 12/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
